3ffd398ebc237a52408273744cdab40d9b765f87
Harichandra-Prasath <1hcp302004@gmail.com>
2024-11-04 08:18:44 +0530

    Simpler Fopen

--- fopen.sh
+++ fopen.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
-                                                                                                │
+
 fpath=$(fd -t f . '/home/hcp_0/' | fzf-tmux -p --reverse)
 
 if [ "$fpath" = "" ]; then
 	exit
 fi
 
-IFS='/'
-read -a paths<<<"$fpath"
 
-p_len=${#paths[@]}
-f_name=${paths[p_len-1]}
-
-
-#If last name contains spaces it needs to be taken care
-IFS='\'
-read -a parts<<<"$f_name"
-
-echo ${parts[@]}
-f_len=${#parts[@]}
-
-echo "$f_len"
+echo "'$fpath'" | xargs code 
