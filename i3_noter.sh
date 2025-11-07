2cdc58748581bcd95e65ea6ad19254f4d13c3703
Harichandra Prasath <1hcp302004@gmail.com>
2025-11-07 21:38:15 +0530

    Font Changes

--- i3_noter.sh
+++ i3_noter.sh
@@ -5,7 +5,7 @@ NB='#1d1f21'
 NF='#c5c8c6'
 SB='#81a2be'
 SF='#1d1f21'
-FONT='Cousine-12'
+FONT='Cousine-14'
 
 
 WDIR=$HOME/texts/
--- menu.sh
+++ menu.sh
@@ -5,7 +5,7 @@ NB='#1d1f21'
 NF='#c5c8c6'
 SB='#81a2be'
 SF='#1d1f21'
-FONT='Cousine-12'
+FONT='Cousine-14'
 
 function cool_dmenu() {
 dmenu -i -fn $FONT -l $L -nb $NB -nf $NF -sb $SB -sf $SF
