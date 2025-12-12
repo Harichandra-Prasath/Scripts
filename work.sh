82bb8613070ac2478ea2dd6c1d76a5c247d4350c
Harichandra Prasath <1hcp302004@gmail.com>
2025-12-12 23:07:19 +0530

    Activating venv on startup

--- work.sh
+++ work.sh
@@ -16,9 +16,19 @@ p_len=${#paths[@]}
 _name=${paths[p_len-1]}
 
 # Create a new session and switch current client
-tmux switchc -t  `tmux -u new-session -c "$dir" -s "$_name" -n "$_name" -dP`
+tmux switchc -t  $(tmux -u new-session -c "$dir" -s "$_name" -n "$_name" -dP)
 
 sleep 0.3
+
+list=(".venv" "venv")
+
+cd "$dir"
+for d in $(fd -t d --max-depth 1 -H); do
+	if [[ "${list[*]}" == *"$d"* ]]; then
+		tmux send-keys -t "$_name":"$_name" ". ${d}/bin/activate" Enter
+		break	
+	fi
+done
 
 tmux send-keys -t "$_name":"$_name" "nvim ." Enter
 
